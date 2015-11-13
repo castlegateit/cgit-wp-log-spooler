7fe73527a1f234ba38acaa3b3576809ddfbcf82e
Andy Reading <andy@castlegateit.co.uk>
2015-11-13 11:27:24 +0000

    V2: Allow custom log directories.
     - No longer requires CGIT WP Contact Form plugin
     - Log paths can be set with a WordPress filter

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# cgit-wp-log-spooler
+# CGIT WP Log Spoller
 
-Castlegate IT WP Log Spooler is a plugin to accompany Castlegate IT WP Contact Form. It adds a menu item for editors and administrators (anyone with the 'edit_pages' capability) which lists all .CSV files found in the directory specified by the Castlegate IT WP Contact Form plugin, making them available to be spooled as downloads.
+Castlegate IT WP Log Spooler allows the download of CSV log files from within the WordPress administration system. It adds a menu item for editors and administrators (anyone with the 'edit_pages' capability) which lists all .CSV files found in log directories specified by using the `cgit_log_spooler` filter, and makes them available to be spooled as downloads.
+
+## Adding log directories
+
+Logs directories are added to a the Log Spooler using the `cgit_log_spooler`
+filter. Example:
+
+    function my_plugin_set_logs() {
+        function my_plugin_logs($logs) {
+
+            $logs['my-custom-logs'] = array(
+                'label' => 'My Custom Logs',
+                'dir'   => '/var/path/my-custom-logs'
+            );
+
+            return $logs;
+        }
+        add_filter('cgit_log_spooler', 'my_plugin_logs');
+    }
+    add_action('init', 'my_plugin_set_logs', 1);
+
+Paths should not contain a trailing slash. The `label` is used to give your logs a title on the download screen. All CSV files found in specified directories will be available for download.
--- cgit-wp-log-spooler.php
+++ cgit-wp-log-spooler.php
@@ -2,124 +2,197 @@
 
 /*
 
-Plugin Name: Castlegate IT WP Contact Form Log Spooler
+Plugin Name: Castlegate IT WP Log Spooler
 Plugin URI: http://github.com/castlegateit/cgit-wp-log-spooler
-Description: Provides a page within WP admin to spool contact form logs. Requires Castlegate IT WP Contact Form
-Version: 1.0
+Description: Provides a page within WP admin to spool any logs.
+Version: 2.0
 Author: Castlegate IT
 Author URI: http://www.castlegateit.co.uk/
 License: MIT
 
 */
 
-// Test for Castlegate IT WP Contact Form by looking for its constant
-if (!class_exists('CGIT_Log_Spooler') && defined('CGIT_CONTACT_FORM_LOG') && is_dir(CGIT_CONTACT_FORM_LOG)) {
+if (!class_exists('CGIT_Log_Spooler')) {
 
-    class CGIT_Log_Spooler {
+    class CGIT_Log_Spooler
+    {
+        /**
+         * Array of log directories available for download. Logs are added to
+         * this array by using the cgit_log_spooler filter. See docs for
+         * full information on filtering this array.
+         *
+         * @var  array
+         */
+        static private $log_dirs = array();
 
-        static function on_load() {
+        /**
+         * Array of available files for download. Populated by scanning log
+         * directories
+         *
+         * @var  array
+         */
+        static private $log_files = array();
+
+        /**
+         * Loader method
+         *
+         * @return void
+         */
+        static function on_load()
+        {
+            // The log directory array is populated by filters
+            self::$log_dirs = apply_filters('cgit_log_spooler', self::$log_dirs);
+
+            // Add WordPress menu
             add_action('admin_menu', array(__CLASS__,'add_download_page'));
-            add_action('plugins_loaded', array(__CLASS__, 'spool_download'));
-        }
 
+            // Scan for logs
+            self::scan_for_logs();
 
-        /**
-         * Add settings page
-         */
-        static function add_download_page () {
-            add_menu_page('Contact Form Logs', 'Contact Forms', 'edit_pages', 'contact-logs', array(__CLASS__, 'contact_logs_page'), '', 74);
+            // Spool any downloads
+            self::spool_download();
         }
 
         /**
-         * Scan the logs directory
-         * @return (array)  array of filename => contact form name
+         * Add WordPress menu page
+         *
+         * @return  void
          */
-        static function scan_logs() {
+        static function add_download_page()
+        {
+            add_menu_page(
+                'Website log downloads',
+                'Website Logs',
+                'edit_pages',
+                'contact-logs',
+                array(__CLASS__, 'logs_page'),
+                '',
+                74
+            );
+        }
 
-            $files = scandir(CGIT_CONTACT_FORM_LOG);
-            $logs = array();
+        /**
+         * Scan all log directories for CSV files
+         *
+         * @return void
+         */
+        static function scan_for_logs()
+        {
+            // Loop through logs array
+            foreach (self::$log_dirs as $key => $settings) {
 
-            foreach ($files as $file)
-            {
-                if (substr($file,-4)=='.csv' && substr($file, 0, 12)=='contact_form')
-                {
-                    $log_name = substr($file, 13, -4);
+                // Disregard anything without a `dir` or `label` index
+                if (!isset($settings['dir']) || !isset($settings['label'])) {
+                    continue;
+                }
 
-                    $logs[$file] = ( $log_name == '0' ? 'Contact Form' : ucwords($log_name) );
+                // Get the CSV files from a directory
+                $files = self::read_directory($settings['dir']);
 
+                // Add to the list of files
+                if ($files) {
+                    self::$log_files[$key] = $files;
                 }
             }
+        }
+
+        /**
+         * Read a log directory for CSV files
+         *
+         * @param  string $directory
+         * @return array
+         */
+        static function read_directory($directory)
+        {
+            $files = scandir($directory);
+
+            $logs = array();
+
+            foreach ($files as $file) {
+                if (substr($file, -4) == '.csv') {
+                    $logs[] = $file;
+                }
+            }
+
             return $logs;
         }
 
-
         /**
          * Generate the download log page
+         *
          * @return void
          */
-        static function contact_logs_page() {
-
-            // What logs do we have? Compile then into an array
-            $files = scandir(CGIT_CONTACT_FORM_LOG);
-            $logs = self::scan_logs();
-
-            foreach ($files as $file)
-            {
-                if (substr($file,-4)=='.csv' && substr($file, 0, 12)=='contact_form')
-                {
-                    $log_name = substr($file, 13, -4);
-
-                    $logs[$file] = ( $log_name == '0' ? 'Contact Form' : ucwords($log_name) );
-
-                }
-            }
+        static function logs_page()
+        {
         ?>
         <div class="wrap">
 
-            <h2>Contact form download logs</h2>
+            <h2>Download site logs</h2>
 
             <p>Download logs in .CSV format suitable for import into spreadsheet applications such as Excel.</p>
 
-            <?php if (count($logs)==0) { ?>
+            <?php if (!self::$log_files) : ?>
 
                 <p>There are no logs currently available for download.</p>
 
-            <?php } else { foreach ($logs as $filename => $log ) : ?>
+            <?php else : ?>
 
-                <p> &bull; <a href="<?php echo $_SERVER['REQUEST_URI']; ?>&amp;cgit_download_log=<?php echo $filename; ?>"><?php echo $log; ?></a></p>
+                <?php foreach (self::$log_files as $key => $files) : ?>
 
-            <?php endforeach; } ?>
+                    <h3><?=self::$log_dirs[$key]['label']?></h3>
+
+                    <?php foreach ($files as $index => $file) : ?>
+
+                        <p> &bull; <a href="<?php echo $_SERVER['REQUEST_URI']; ?>&amp;cgit_log_key=<?=$key; ?>&amp;cgit_log_index=<?=$index?>"><?php echo $file; ?></a></p>
+
+                    <?php endforeach ?>
+
+                <?php endforeach ?>
+
+            <?php endif ?>
 
         </div><?php
         }
 
-
         /**
-         * Spool a CSV download if one has been requested
+         * Spool a CSV download if one has been requested.
          */
-        static function spool_download() {
-
+        static function spool_download()
+        {
             global $pagenow;
 
-            // What logs do we have? Compile then into an array
-            $files = scandir(CGIT_CONTACT_FORM_LOG);
-            $logs = self::scan_logs();
-
-            if ($pagenow=='admin.php' &&
-                  isset($_GET['cgit_download_log'])  &&
+            if ($pagenow == 'admin.php' &&
+                  isset($_GET['cgit_log_key']) &&
+                  isset($_GET['cgit_log_index']) &&
                   current_user_can('edit_pages') &&
-                  array_key_exists($_GET['cgit_download_log'], $logs)) {
+                  array_key_exists($_GET['cgit_log_key'], self::$log_dirs) &&
+                  array_key_exists($_GET['cgit_log_index'], self::$log_files[$_GET['cgit_log_key']])
+            ) {
+
+                // Build the filename and path
+                $path = self::$log_dirs[$_GET['cgit_log_key']]['dir'] . '/';
+                $filename = self::$log_files[$_GET['cgit_log_key']][$_GET['cgit_log_index']];
 
                 header("Content-type: text/csv");
-                header("Content-Disposition: attachment; filename=" . $_GET['cgit_download_log']);
+                header("Content-Disposition: attachment; filename=" . $filename);
                 header("Pragma: no-cache");
                 header("Expires: 0");
-                readfile(CGIT_CONTACT_FORM_LOG . $_GET['cgit_download_log']);
+                readfile($path . $filename);
                 exit();
             }
         }
     }
 
-    CGIT_Log_Spooler::on_load();
+    /**
+     * Delay the loading of the plugin until the `init` event to allow for
+     * logs to be added to the array
+     *
+     * @return void
+     */
+    function cgit_log_spool_wait()
+    {
+        CGIT_Log_Spooler::on_load();
+    }
+    add_action('init', 'cgit_log_spool_wait', 999);
 
 }
